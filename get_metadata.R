3bc0962eff6e4104ce34a8f103af48c507a183ac
kevgar <kjgardner74@gmail.com>
2018-01-22 21:56:21 -0500

    Updated code and added csv file for civil forfeiture data

--- get_metadata.R
+++ get_metadata.R
@@ -1,12 +1,13 @@
-get_query <- function(begin_date="01/17/2018", end_date="01/17/2018", case_type="CD"){
+get_query <- function(start_dayMonthYear, end_dayMonthYear, case_type){
     
-    # begin_date <- "01/17/2018"
-    # end_date <- "01/17/2018"
+    # Properly format user inputs for the query url
+    start_dayMonthYear <- gsub(pattern="[-. ]", replacement = "/", start_dayMonthYear)
+    end_dayMonthYear <- gsub(pattern="[-. ]", replacement = "/", end_dayMonthYear)
+    case_type <- gsub(pattern=" ", replacement = "%20", case_type)
     
     baseURL <- "https://caseinfo.aoc.arkansas.gov/cconnect/PROD/public/ck_public_qry_doct.cp_dktrpt_new_case_report?backto=C&case_id="
     
-    
-    queryURL <- paste0("&begin_date=", begin_date, "&end_date=", end_date,
+    queryURL <- paste0("&begin_date=", start_dayMonthYear, "&end_date=", end_dayMonthYear,
                        "&county_code=ALL&cort_code=ALL&locn_code=ALL&case_type=",
                        case_type,"&docket_code=")
     
@@ -19,34 +20,50 @@ get_query <- function(begin_date="01/17/2018", end_date="01/17/2018", case_type=
 # Get data for specified time period and case type
 library(rvest)
 
-doc <- read_html(get_query("01/01/2017","01/07/2017", "CF%20-%20PROPERTY%20FORFEITURE"), options = "HUGE")
-# doc <- read_html(get_query("01/18/2017","01/18/2018", "CF%20-%20PROPERTY%20FORFEITURE"), options = "HUGE")
+start_dayMonthYear <- "04/01/1989"
+end_dayMonthYear <- "01/20/2018"
+case_type <- "CF - PROPERTY FORFEITURE"
 
-# Extract column names
-columnNames <- html_nodes(doc, "th") %>% html_text()
-# columnNames
+# Query the url
+doc <- read_html(get_query(start_dayMonthYear,end_dayMonthYear,case_type),options = "HUGE")
+
+# Get the column names
+html_nodes(doc, "th") %>% html_text()
 # [1] "Case Action Filing Date"
 # [2] "Case Id / Description"  
 # [3] "Case Type Description"  
 # [4] "Judge"                  
 # [5] "Court (Court/Location)" 
 
+# Get data from "Cases Filed by Date Search" results
+case_action_filing_date    <- html_nodes(doc, "td:nth-child(1)") %>% html_text() %>% .[-c(1:6)]
+case_action_filing_date    <- as.Date(case_action_filing_date, format="%d-%B-%y")
+temp                       <- html_nodes(doc, "td:nth-child(2)") %>% html_text() %>% .[-c(1:6)] 
+case_id                    <- regmatches(temp,regexpr("(^[^ ]+)", temp))
+case_description           <- trimws(gsub(pattern = "(^[^ ]+)",replacement="",temp))
+pat                        <- "\\$\\(?[0-9.,]+\\)?"
+areDollars                 <- which(grepl(pat, case_description, perl=TRUE))
+dollar_ammount             <- rep(NA_character_, length(case_description))
+dollar_ammount[areDollars] <- regmatches(case_description,regexpr(pat,case_description))
+dollar_ammount[areDollars] <- gsub(pattern="[$,]",replacement="",dollar_ammount[areDollars],perl=TRUE)
+case_type_description      <- html_nodes(doc, "td:nth-child(3)") %>% html_text() %>% .[-c(1:6)]
+judge                      <- html_nodes(doc, "td:nth-child(4)") %>% html_text() %>% trimws(which="both")
+court_location             <- html_nodes(doc, "td:nth-child(5)") %>% html_text()
 
-# Extract data for each column
-case_action_filing_date <- html_nodes(doc, "td:nth-child(1)") %>% html_text() %>% .[-c(1:6)]
-temp                    <- html_nodes(doc, "td:nth-child(2)") %>% html_text() %>% .[-c(1:6)] 
-case_id                 <- trimws(substr(temp,1,10), "both")
-case_description        <- trimws(substr(temp,11,nchar(temp)), "both")
-pat                     <- "\\$\\(?[0-9.,]+\\)?"
-hasDollars              <- grepl(pattern = pat, x = case_description)
-dollar_ammount          <- ifelse(hasDollars,
-                                  regmatches(case_description,regexpr(pat,case_description)),
-                                  NA_character_)
-dollar_ammount          <- gsub(pattern="[$,]", replacement="", dollar_ammount, perl=TRUE)
-case_type_description   <- html_nodes(doc, "td:nth-child(3)") %>% html_text() %>% .[-c(1:6)]
-judge                   <- html_nodes(doc, "td:nth-child(4)") %>% html_text() %>% trimws(which="both")
-court_location          <- html_nodes(doc, "td:nth-child(5)") %>% html_text()
+# Get data from docket reports
+docket_report_url          <- paste0("https://caseinfo.aoc.arkansas.gov/cconnect/PROD/public/ck_public_qry_doct.cp_dktrpt_frames?backto=C&case_id=",case_id,"&begin_date=&end_date=")
 
+# test
+read_html(docket_report_url[2]) #%>% html_nodes("table, tbody, tr, td")
+
+# if case was monetary, extract the claimant name
+# claimant <- 
+# if case was monetary, extract the claimant name
+# claimant_id <- 
+
+# defendant_name
+# defendant_id
+# judge_id
 
 # Create dataframe
 df <- data.frame(
@@ -56,11 +73,9 @@ df <- data.frame(
     dollar_ammount=as.numeric(dollar_ammount),
     case_type_description=case_type_description,
     court_location=court_location,
+    docket_report_url=docket_report_url,
     stringsAsFactors = F)
 
-
-# Write dataframe to csv
-write.csv(df,"AOC_Extract.csv", row.names = FALSE)
-
-# Convert list of row elements to dataframe
-
+# Write dataframe to .csv
+outFileName <- gsub(pattern = "/",replacement="-",paste0("AOC_Extract_",start_dayMonthYear,"_",end_dayMonthYear,".csv"), fixed=T)
+write.csv(df,outFileName, row.names = FALSE)
